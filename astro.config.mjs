f171d411d10fbec68fb7582d28382cbe00fc4ac9
Edwin Vera <97845465+veraedwinem@users.noreply.github.com>
2026-05-13 00:19:15 -0600

    Fix Vercel adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   redirects: {
     '/conferenciafiel2026':
       'https://www.eventbrite.com.mx/e/conferencia-fiel-2026-tickets-1983624421041?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=ebdsshcopyurl',
